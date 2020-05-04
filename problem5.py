1752a99de49c623dfb2b5dc8816e748dc00f0d1f
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2020-05-05 01:15:52 +0530

    Problem 5 - Better readability

--- problem5.py
+++ problem5.py
@@ -5,7 +5,7 @@
 lcm = 1
 
 def sieveEratosthenes(n):
-    c= []
+    array = []              #array of primes
     prime = [True for i in range(n+1)]
     p = 2 
     while(p*p <= n):
@@ -17,41 +17,41 @@ def sieveEratosthenes(n):
     
     for p in range(2,n):
         if prime[p]:
-            c.append(p)
-    return c
+            array.append(p)
+    return array
 
 # For any number
 # n = int(input("Enter number 'n' to print lcm of first n natural numbers:"))
 
 
-c = sieveEratosthenes(20)                       #n for 20
+primes = sieveEratosthenes(20)                  #n for 20
 numbers = [x for x in range(1,21)]              #n+1 for 21
 
         
-print(numbers)       
+print("The numbers are",numbers)       
         
 
-print(range(len(c)))
 
-for i in c:
+
+for i in primes:                                #loops through the primes before 20
     
-    count = [0 for x in range(len(numbers))]
+    count = [0 for x in range(len(numbers))]    #counts the power of a prime in the prime factorisation of a number
     
-    j = 0
+    print("Prime number: ", i)    
     
     for j in range(len(numbers)):
         
         if numbers[j]%i==0:
-            print("now",numbers[j],"on repeated division with",i)
+            print("\tNow",numbers[j],"on repeated division with",i)
             while(numbers[j]/i>=1 and numbers[j]%i==0):
                 numbers[j]/=i
-                count[j]+=1
+                count[j]+=1                     #count -> read prev comment
                 
-            print("becomes",numbers[j])
+            print("\t\tbecomes",numbers[j])
             
             
         
-    print("max = ",max(count))
+    print("\nMax power of %d = "%(2),max(count))
     for x in range(max(count)):
         lcm*=i        
     print(i,numbers,"\n\n")
